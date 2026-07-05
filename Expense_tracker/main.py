e9a80575cfffb5f8601a91b67962aba75f2134e9
Aryan Kumar <aryankumar17114@gmail.com>
2026-07-05 18:36:31 +0530

    write the actual duty for main

--- Expense_tracker/main.py
+++ Expense_tracker/main.py
@@ -1,10 +1,20 @@
 from expense import Expense as ex
 from tracker import ExpenseTracker as et
 tracker =et()
-exp1=ex(40,"food","milk","29-06-2026")
-exp2=ex(50,"food","jalebi","5-07-26")
-exp3=ex(60,"food","sutte","5-07-26")
-tracker.add_expense(exp1)
-tracker.add_expense(exp2)
-tracker.add_expense(exp3)
-tracker.view_expense()
+while(True):
+    print("Starting the tracker")
+    menu=int(input("Enter the choice : 1/2/3: "))
+    if menu==1:
+        amount=int(input(" "))
+        category=input(" ")
+        description=input(" ")
+        date=input(" ")
+        exp=ex(amount,category,description,date)
+        tracker.add_expense(exp)
+    elif menu==2 :
+        tracker.view_expense()
+    elif menu==3 :
+        print("Exiting")
+        break
+    else : 
+        print("invalid input")
